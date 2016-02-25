3bd3b013cbd4c64ce76b360077d26ca647fd879f
Jonathan Randall <jonathanrandall@Jonathans-MacBook-Air.local>
2016-02-24 21:20:30 -0800

    added gradient changes to Map.showHeatMap function in map.js

--- scripts/model/map.js
+++ scripts/model/map.js
@@ -24,8 +24,25 @@ Map.showBikeLayer = function(){
 };
 
 Map.showHeatMap = function() {
+  var g= [
+    'rgba(0, 255, 255, 0)',
+'rgba(0, 255, 255, 1)',
+'rgba(0, 191, 255, 1)',
+'rgba(0, 127, 255, 1)',
+'rgba(0, 63, 255, 1)',
+'rgba(0, 0, 255, 1)',
+'rgba(0, 0, 223, 1)',
+'rgba(0, 0, 191, 1)',
+'rgba(0, 0, 159, 1)',
+'rgba(0, 0, 127, 1)',
+'rgba(63, 0, 91, 1)',
+'rgba(127, 0, 63, 1)',
+'rgba(191, 0, 31, 1)',
+'rgba(255, 0, 0, 1)'
+  ]
   var heatmap = new google.maps.visualization.HeatmapLayer({
-    data: heatMapData()
+    data: heatMapData(),
+    gradient: g
   });
   heatmap.setMap(map);
 };
